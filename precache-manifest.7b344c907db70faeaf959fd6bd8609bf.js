c134073c1b8144d4e3c7aa1a2f57213a3d6abf60
Aneesh <aneeshpk93@gmail.com>
2019-03-26 13:52:52 +0000

    Updates

---
+++ precache-manifest.7b344c907db70faeaf959fd6bd8609bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2add347144c7bce3ed25",
+    "url": "/diagnal-react/static/css/main.336667bd.chunk.css"
+  },
+  {
+    "revision": "2add347144c7bce3ed25",
+    "url": "/diagnal-react/static/js/main.313f02bf.chunk.js"
+  },
+  {
+    "revision": "95009ba1a6833dc8e354",
+    "url": "/diagnal-react/static/js/runtime~main.4bf3f453.js"
+  },
+  {
+    "revision": "5756b183c97116952408",
+    "url": "/diagnal-react/static/js/2.d8afc5a9.chunk.js"
+  },
+  {
+    "revision": "c1b6a185b017a9eb5cc0cef7e06f38b0",
+    "url": "/diagnal-react/index.html"
+  }
+];
