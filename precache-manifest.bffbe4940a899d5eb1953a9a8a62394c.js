93ad78d1c8d68c6275366e097c38b8fc95077de6
Aneesh <aneeshpk93@gmail.com>
2019-03-27 10:14:22 +0000

    Updates

---
+++ precache-manifest.bffbe4940a899d5eb1953a9a8a62394c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17fe7ce7a263272f08a0",
+    "url": "/lazy/static/css/main.7fdf4d1a.chunk.css"
+  },
+  {
+    "revision": "17fe7ce7a263272f08a0",
+    "url": "/lazy/static/js/main.2df1322e.chunk.js"
+  },
+  {
+    "revision": "432356865c7e6538d3e4",
+    "url": "/lazy/static/js/runtime~main.c43f7a55.js"
+  },
+  {
+    "revision": "94722b573b088082e1bd",
+    "url": "/lazy/static/js/2.c2ad270c.chunk.js"
+  },
+  {
+    "revision": "445a00ef0eb5bcad9e52263ad9b9740c",
+    "url": "/lazy/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/lazy/precache-manifest.b0f0e554c47301b718802b81f20cab08.js"
+  "/lazy/precache-manifest.bffbe4940a899d5eb1953a9a8a62394c.js"
 );
 
 workbox.clientsClaim();
